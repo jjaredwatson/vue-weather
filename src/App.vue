78e5e41db8c9efb7ef95fa18e8e7f666ff8cfc97
Jared Watson <watsonj@ITJW20.local>
2020-08-27 11:09:52 -0500

    js and fixes to interractive css added

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 72 ? 'warm' : ''">
     <main>
 
       <div class="search-box">
@@ -12,16 +12,16 @@
         />
       </div>
 
-      <div class="weather-wrap">
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
-          <div class="location">Springfield, IL</div>
-          <div class="date">Wednesday, September 26, 2020</div>
+          <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
+          <div class="date">{{ dateBuilder() }}</div>
         </div>
-      </div>
 
-      <div class="weather-box">
-        <div class="temp">88°</div>
-        <div class="weather">Rain</div>
+        <div class="weather-box">
+          <div class="temp">{{ Math.round(weather.main.temp)}}°f</div>
+          <div class="weather">{{ weather.weather[0].main }}</div>
+        </div>
       </div>
 
     </main>
@@ -40,8 +40,28 @@ export default {
     }
   },
   methods: {
-    fetchWeather () {
-      
+    fetchWeather (e) {
+      if (e.key == "Enter") {
+        fetch(`${this.url_base}weather?q=${this.query}&units=imperial&APPID=${this.api_key}`)
+          .then(res => {
+            return res.json();
+          }).then(this.setResults);
+      }
+    },
+    setResults (results) {
+      this.weather = results;
+    },
+    dateBuilder () {
+      let d = new Date();
+      let months = ["January", "February", "March", "April", "May", "June", "July", "July", "August", "September", "October", "November", "December"];
+      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+
+      return `${day}, ${month} ${date}, ${year}`;
     }
   }
 }
@@ -63,6 +83,10 @@ body {
   background-size: cover;
   background-position: bottom;
   transition: 0.4s;
+}
+
+#app.warm {
+  background-image: url("./assets/warm-bg.jpg");
 }
 
 main {
